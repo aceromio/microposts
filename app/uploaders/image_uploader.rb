ec2a8da5faf2926022f4c41dbe7a3d82c8bf2e8c
aceromio <rikishirao@gmail.com>
2015-12-23 08:15:49 +0000

    carrier totyuu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,30 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
+  process :resize_to_limit => [700, 700]
+
+
+  process :convert => 'jpg'
+
+  version :thumb do
+    process :resize_to_limit => [300, 300]
+  end
+
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+
+  def filename
+    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+  end
+  def filename
+    time = Time.now
+    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
+    name.downcase
+  end
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
