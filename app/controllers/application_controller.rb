37d985f17504104a53aaaf24d89b33b1797fdd92
aceromio <rikishirao@gmail.com>
2016-01-05 10:00:31 +0000

    iji

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  def set_locale
+  I18n.locale = extract_locale_from_tld || I18n.default_locale
+  end
+  def extract_locale_from_tld
+  parsed_locale = request.host.split('.').last
+  I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+  end
 
   private
   def logged_in_user
